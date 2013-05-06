5547269604c281cd33a6843de0f28381ae217db9
rongzheyi <rongzheyi@gmail.com>
2013-05-06 13:53:48 +0200

    deleted unused Config parameters

--- ds/src/main/java/util/Config.java
+++ ds/src/main/java/util/Config.java
@@ -6,11 +6,10 @@ public class Config
 	public static final boolean DEBUG_MODE = true;
 	
 	// internal configuration
-	public static final String LEFT_PATH = "dps.left.path";		// the left path of data set. directory or file.
-	public static final String RIGHT_PATH = "dps.right.path";
-	public static final String N_SAMPLES = "dps.sample.size";
-	public static final String RIGHT_LINE_LENGTH = "dps.right.line.length";
-	public static final String LONGEST_LINE_LENGTH = "dps.longest.line.length";
+	public static final String RIGHT_PATH = "dps.right.path";					// used in disc
+	public static final String N_SAMPLES = "dps.sample.size";					// used in mappers
+	public static final String RIGHT_LINE_LENGTH = "dps.right.line.length";		// used in disc
+	public static final String LONGEST_LINE_LENGTH = "dps.longest.line.length";	// used in expander
 	
 	// can be explicitly set
 	public static final String MIN_PATTERN_LENGTH = "dps.min.pattern.length";
@@ -18,9 +17,7 @@ public class Config
 	// default values
 	public static final int DEFAULT_MIN_PATTERN_LENGTH = 0; // adjustable
 	public static final int DEFAULT_MAX_RECORD_LENGTH = 50; // adjustable
-	
-	public static final int DEFAULT_LEFT_BUFFER_LINES = 1000;	// number of lines that a mapper will read once
-		
+			
 	// separators
 	public final static String SepItems = " ";
 	public final static String SepItemsRegex = " +";
